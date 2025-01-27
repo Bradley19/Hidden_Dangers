2da44e60e7e5ea2aa71e140dd18c3b5aedc2223b
Linkbrad <Linkbrad3167@gmail.com>
2025-01-27 01:07:29 -0700

    Fixed Broken recipies

--- src/main/java/net/linkbrad/hiddendangers/block/ModBlocks.java
+++ src/main/java/net/linkbrad/hiddendangers/block/ModBlocks.java
@@ -4,15 +4,28 @@ import net.linkbrad.hiddendangers.HiddenDangers;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.enums.NoteBlockInstrument;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+import javax.swing.text.html.HTML;
+
 public class ModBlocks {
     public static final Block TARBLOCK = registerBlock("tar_block",
-            new Block(AbstractBlock.Settings.copy(Blocks.DIRT)));
+            new Block(AbstractBlock.Settings.create()
+                    .requiresTool()
+                    .noCollision()
+                    .burnable()
+                    .instrument(NoteBlockInstrument.SNARE)
+                    .hardness(10.0f)
+                    .velocityMultiplier(1.2f)
+                    .jumpVelocityMultiplier(0.8f)
+                    .pistonBehavior(PistonBehavior.NORMAL)));
 
 
 
